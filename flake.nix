bc71f03e4c6385b4ea571c967387ff8ce3cfbaea
michaelmnemonic <michaelmnemonic@posteo.eu>
2026-01-12 19:36:09 +0100

    churn: format

--- flake.nix
+++ flake.nix
@@ -16,35 +16,33 @@
       pkgs = nixpkgs.legacyPackages.${system}.pkgs;
     in {
       vscode = pkgs.vscode-with-extensions.override {
-          vscodeExtensions = [
-            self.packages.${system}.syside
-          ];
+        vscodeExtensions = [
+          self.packages.${system}.syside
+        ];
+      };
+      syside = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
+        mktplcRef = {
+          name = "syside-editor";
+          publisher = "sensmetry";
+          version = "0.8.3";
+          sha256 = "sha256-mZGtB1BaeVIR4K/cT0elpfW7zrQBFScVHHOsHIuKvJg=";
+          arch = "linux-x64";
         };
-        syside = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-          mktplcRef = {
-            name = "syside-editor";
-            publisher = "sensmetry";
-            version = "0.8.3";
-            sha256 = "sha256-mZGtB1BaeVIR4K/cT0elpfW7zrQBFScVHHOsHIuKvJg=";
-            arch = "linux-x64";
-          };
-          nativeBuildInputs = [pkgs.autoPatchelfHook];
-          buildInputs = [pkgs.stdenv.cc.cc.lib];
-          # installPhase = ''
-          #   tar -xJf ../syside-0.8.3-x86_64-linux-glibc.tar.xz -C $out/share/vscode/extensions/sensmetry.syside-editor/dist
-          # '';
-        };
+        nativeBuildInputs = [pkgs.autoPatchelfHook];
+        buildInputs = [pkgs.stdenv.cc.cc.lib];
+        # installPhase = ''
+        #   tar -xJf ../syside-0.8.3-x86_64-linux-glibc.tar.xz -C $out/share/vscode/extensions/sensmetry.syside-editor/dist
+        # '';
+      };
     });
     devShell = forAllSystems (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
-
       in
         pkgs.mkShell {
-          buildInputs = with pkgs;
-            [
-              alejandra
-            ];
+          buildInputs = with pkgs; [
+            alejandra
+          ];
           shellHook = ''
             alias code='NIXPKGS_ALLOW_UNFREE=1 nix run --impure .#vscode -- '
           '';
